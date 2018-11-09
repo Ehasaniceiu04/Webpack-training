27bbab5465ffd30d1b8771b9d251d6550be6713b
Ehasanul Hoque <ehasan@outlook.com>
2018-11-10 01:17:18 +0600

    loaders added

---
+++ loaders/login.es6
@@ -0,0 +1,7 @@
+let login=(username,password)=>{
+    if(username!=='ehasan' || password!=='mome')
+    {
+        console.log('incorrect login');
+    }
+}
+login('ehasan','hater');
---
+++ loaders/webpack.config.js
@@ -0,0 +1,19 @@
+module.exports={
+    entry:["./utils","./app.js"],
+    output:{
+        filename:'bundle.js'
+    },
+    watch:true,
+    module:{
+        rules:[{
+            test:/\.es6$/,
+            exclude:/node_modules/,
+            loader:"babel-loader"
+        }
+
+     ]
+    },
+    resolve:{
+        extensions:['.js','.es6']
+    }
+}
